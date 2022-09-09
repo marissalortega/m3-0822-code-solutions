e38f8fe312d92e9c5e6ecff268849ed4386629b1
Marissa Ortega <marissaljames@gmail.com>
2022-09-09 02:11:55 +0000

    refactored code

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -13,21 +13,23 @@ class HotButton extends React.Component {
 
   render() {
     const counter = this.state.count;
-    if (counter < 3) {
-      return <button onClick={this.handleClick}>Hot Button</button>;
-    } else if (counter >= 3 && counter < 6) {
-      return <button onClick={this.handleClick} className="dark-purple">Hot Button</button>;
+    let btnClass = '';
+
+    if (counter >= 3 && counter < 6) {
+      btnClass = 'dark-purple';
     } else if (counter >= 6 && counter < 9) {
-      return <button onClick={this.handleClick} className="purple">Hot Button</button>;
+      btnClass = 'purple';
     } else if (counter >= 9 && counter < 12) {
-      return <button onClick={this.handleClick} className="red">Hot Button</button>;
+      btnClass = 'red';
     } else if (counter >= 12 && counter < 15) {
-      return <button onClick={this.handleClick} className="orange">Hot Button</button>;
+      btnClass = 'orange';
     } else if (counter >= 15 && counter < 18) {
-      return <button onClick={this.handleClick} className="yellow">Hot Button</button>;
+      btnClass = 'yellow';
     } else if (counter >= 18) {
-      return <button onClick={this.handleClick} className="white">Hot Button</button>;
+      btnClass = 'white';
     }
+
+    return <button onClick={this.handleClick} className={btnClass}>Hot Button</button>;
   }
 }
 
