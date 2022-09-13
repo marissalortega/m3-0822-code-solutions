38603de1edfa0775867f87c69f95d61a62b647a5
Marissa Ortega <marissaljames@gmail.com>
2022-09-13 04:39:39 +0000

    worked on react-validated-input-component

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,7 @@
+import ValidatedPassword from './validated-input';
+import ReactDOM from 'react-dom/client';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(ValidatedPassword);
---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class ValidatedPassword extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { password: '' };
+
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+
+  }
+
+  render() {
+    const passwordLength = this.state.password.length;
+    let errorMsg = '';
+    let iconClass = '';
+    let errorColor = '';
+
+    if (passwordLength === 0) {
+      errorMsg = 'A password is required';
+      iconClass = 'fa-solid fa-xmark fa-xl red';
+      errorColor = 'red';
+    } else if (passwordLength > 0 && passwordLength < 8) {
+      errorMsg = 'Your password is too short.';
+      iconClass = 'fa-solid fa-xmark fa-xl red';
+      errorColor = 'red';
+    } else {
+      errorMsg = '';
+      iconClass = 'fa-solid fa-check fa-xl green';
+      errorColor = 'green';
+    }
+
+    return (
+    <form>
+      <label>Password:</label>
+      <input
+        onChange={this.handlePasswordChange}
+        type="password"
+        name="password"
+        value={this.setState.password}
+        />
+        <i className={iconClass}></i>
+        <p className={errorColor}>{errorMsg}</p>
+    </form>
+    );
+  }
+
+}
+
+export default <ValidatedPassword />;
