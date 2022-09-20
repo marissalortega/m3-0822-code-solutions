fde442ad9fa87a01f68c94423e02b8aea1eb1e0b
Marissa Ortega <marissaljames@gmail.com>
2022-09-20 02:41:04 +0000

    progress made on react-accordion-component

---
+++ react-accordion-component/src/index.jsx
@@ -0,0 +1,7 @@
+import Accordion from './accordion';
+import ReactDOM from 'react-dom/client';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(Accordion);
