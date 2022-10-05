aeccac5b7191a5a7ac065437ad7462397588c4a4
Marissa Ortega <marissaljames@gmail.com>
2022-10-05 03:49:26 +0000

    completed react-context

---
+++ react-context/client/components/page-container.jsx
@@ -0,0 +1,20 @@
+import AppContext from '../lib/app-context';
+import React from 'react';
+
+const styles = {
+  page: {
+    minHeight: 'calc(100vh - 3.5rem)'
+  }
+};
+
+export default function PageContainer({ children }) {
+  return (
+    <div className="bg-light">
+      <div className="container" style={styles.page}>
+        { children }
+      </div>
+    </div>
+  );
+}
+
+PageContainer.contextType = AppContext;
